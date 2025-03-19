f567894437b531f844cad7823e2e4d6eaa7a7614
tcaceresm <caceres.tomas99@gmail.com>
2025-03-19 00:26:00 -0300

    Added ligand ID to docking scores csv

--- scripts/docking/process_docking/ligandWise/process_dlg.R
+++ scripts/docking/process_docking/ligandWise/process_dlg.R
@@ -37,21 +37,26 @@ docking_output <- read.SDFset(sdfstr = ligand_sdf)
 # Processing --------------------------------------------------------------
 
 
-free_energies <- data.frame(Pose=numeric(),
+free_energies <- data.frame(LigandName=character(),
+                            Pose=numeric(),
                             FreeEnergy=numeric())
 
 for (pose in 1:length(docking_output)) {
   
   json_data <- fromJSON(docking_output[[pose]][[4]])
   free_energy <- json_data$free_energy
-  free_energies[pose,] <- c(pose, free_energy)
+  free_energies[pose,] <- list(ligand_name, pose, free_energy)
 
 }
 
 sorted_docking_output <- free_energies[order(free_energies$FreeEnergy), ]
 
 write.SDF(docking_output[sorted_docking_output$Pose],
-          file = sprintf('%s/%s_sorted.sdf', output_path, ligand_name))
+         file = sprintf('%s/%s_sorted.sdf', output_path, ligand_name))
+
+write.SDF(docking_output[sorted_docking_output$Pose][[1]],
+          file = sprintf('%s/%s_best_pose.sdf', output_path, ligand_name))
+
 write.table(x = sorted_docking_output,
           file = sprintf('%s/%s_docking_scores_sorted.csv', output_path, ligand_name),
           row.names = F, col.names = F, sep = ";")
